2208ba52ded86968429e41234c1cea95d77f0617
Phan The Hoa <64406303+thehoap@users.noreply.github.com>
2020-09-17 22:53:21 +0700

    ĐỌC FILE
    Do các chức năng nhập của m đều ở trong Date và Customer, nên m không thể đọc ở trong main đc
    Vấn đề nữa là khi nhập thông tin khách hàng ở Customer thì tới lúc nhập ngày ở setDate thì phải đọc file ở trong setDate => không thể đọc 1 mạch từ 1 file. Cái này chắc phải hỏi cô.

--- ĐỒ ÁN 18.10C/MainQLKS/MainQLKS/MainQLKS.cpp
+++ ĐỒ ÁN 18.10C/MainQLKS/MainQLKS/MainQLKS.cpp
@@ -3,31 +3,8 @@
 #include <fstream>
 int main()
 {
-	/*fstream ifs("c:\\users\\hp\\desktop\\-n-qu-n-l-kh-ch-s-n\\đồ án 18.10c\\mainqlks\\mainqlks\\danhsach.txt");
-	customer* customers = new customer[10];
-	string mahoadon, ten, cmnd, tenphong, loaiphong;
-	bool gioitinh;
-	date ngayden, ngaydi;
-	int tienphong, dongia;
-	size_t i = 0;
-	while (!ifs.eof()) {
-		cin.ignore();
-		ifs >> mahoadon;
-		ifs >> ten;
-		ifs >> cmnd;
-		ifs >> gioitinh;
-		ifs >> tenphong;
-		ifs >> ngayden.setday();
-		ifs >> ngaydi;
-
-	}*/
 	int choice;
-	//x.nhap();
-	//x.xuat();
 	ListCustomer listTry;
-	
-	//listTry.Add();
-	//listTry.Indanhsach();
 	do {	
 		Customer x;
 		cout << "Nhap cong viec can thuc hien: ";
